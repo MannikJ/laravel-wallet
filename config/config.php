bbda34ed80f8ac75beb35e8186d115c1b7b59a60
MannikJ <mannikj@web.de>
2019-06-06 13:36:04 +0200

    Remove unnecessary env variables

--- config/config.php
+++ config/config.php
@@ -18,12 +18,12 @@ return [
     /**
      * Change this if you need to extend the default Wallet Model
      */
-    'wallet_model' => env('WALLET_MODEL', Wallet::class),
+    'wallet_model' => Wallet::class,
 
     /**
      * Change this if you need to extend the default Transaction Model
      */
-    'transaction_model' => env('WALLET_TRANSACTION_MODEL', Transaction::class),
+    'transaction_model' => Transaction::class,
 
     /**
      * Activate automatic recalculation of the wallet balance
@@ -36,13 +36,13 @@ return [
      * Transaction types that are subtracted from the wallet balance.
      * All amounts will be converted to a positive value
      */
-    'adding_transaction_types' => explode(',', env('WALLET_ADDING_TRANSACTION_TYPES', 'deposit,refund')),
+    'adding_transaction_types' => ['deposit', 'refund'],
 
     /**
      * Transaction types that are subtracted from the wallet balance
      * All amounts will be converted to a negative value
      */
-    'subtracting_transaction_types' => explode(',', env('WALLET_SUBTRACTING_TRANSACTION_TYPES', 'withdraw,payout')),
+    'subtracting_transaction_types' => ['withdraw', 'payout'],
 
     /**
      * Transaction types that can be positive or negative
@@ -51,5 +51,5 @@ return [
      * It still might be convenient to specify your the available
      * unbiased types here explicitly so you can show the options
      */
-    'unbiased_transaction_types' => explode(',', env('WALLET_UNBIASED_TRANSACTION_TYPES', '')),
+    'unbiased_transaction_types' => [],
 ];
